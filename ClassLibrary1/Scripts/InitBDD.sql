df387c9d8ac2c98da60a2a6947e834eadde09b81
Eric <eric.guilbaud@gmail.com>
2023-01-18 11:18:13 +0100

    jdd

--- ClassLibrary1/Scripts/InitBDD.sql
+++ ClassLibrary1/Scripts/InitBDD.sql
@@ -3,3 +3,10 @@ INSERT INTO Product (Name,Price,ProductCategory) VALUES
 ('burger',15.5, 1),
 ('chips',15.5, 1),
 ('orangine',15.5, 1)
+
+INSERT INTO Menu (Name) VALUES 
+('menu gras'),
+('menu gras++')
+
+INSERT INTO Menu_Product(MenuId, ProductId) VALUES 
+(1, 1), (1,2), (2,1), (2,1), (2,2), (2,2)
