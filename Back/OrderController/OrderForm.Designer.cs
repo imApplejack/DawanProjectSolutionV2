b5924c9adc028ab3a2fd0b2071c76962028ccf8e
Matthieu <matthieu.chevalier.pro@gmail.com>
2023-01-23 07:50:26 +0100

    IOrder couche présentation1

--- Back/OrderController/OrderForm.Designer.cs
+++ Back/OrderController/OrderForm.Designer.cs
@@ -28,31 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listViewCategories = new System.Windows.Forms.ListView();
-            this.listViewMenusProduits = new System.Windows.Forms.ListView();
             this.textBoxOrderName = new System.Windows.Forms.TextBox();
             this.dataGridOrder = new System.Windows.Forms.DataGridView();
             this.textBoxTotal = new System.Windows.Forms.TextBox();
             this.buttonAttente = new System.Windows.Forms.Button();
             this.buttonPayer = new System.Windows.Forms.Button();
+            this.listBoxCategories = new System.Windows.Forms.ListBox();
+            this.listBoxProduitMenu = new System.Windows.Forms.ListBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridOrder)).BeginInit();
             this.SuspendLayout();
-            // 
-            // listViewCategories
-            // 
-            this.listViewCategories.Location = new System.Drawing.Point(24, 52);
-            this.listViewCategories.Name = "listViewCategories";
-            this.listViewCategories.Size = new System.Drawing.Size(157, 328);
-            this.listViewCategories.TabIndex = 0;
-            this.listViewCategories.UseCompatibleStateImageBehavior = false;
-            // 
-            // listViewMenusProduits
-            // 
-            this.listViewMenusProduits.Location = new System.Drawing.Point(199, 52);
-            this.listViewMenusProduits.Name = "listViewMenusProduits";
-            this.listViewMenusProduits.Size = new System.Drawing.Size(176, 327);
-            this.listViewMenusProduits.TabIndex = 1;
-            this.listViewMenusProduits.UseCompatibleStateImageBehavior = false;
             // 
             // textBoxOrderName
             // 
@@ -95,18 +79,37 @@
             this.buttonPayer.Text = "Payer";
             this.buttonPayer.UseVisualStyleBackColor = true;
             // 
+            // listBoxCategories
+            // 
+            this.listBoxCategories.FormattingEnabled = true;
+            this.listBoxCategories.ItemHeight = 15;
+            this.listBoxCategories.Location = new System.Drawing.Point(38, 52);
+            this.listBoxCategories.Name = "listBoxCategories";
+            this.listBoxCategories.Size = new System.Drawing.Size(120, 94);
+            this.listBoxCategories.TabIndex = 6;
+            this.listBoxCategories.SelectedIndexChanged += new System.EventHandler(this.listBoxCategories_SelectedIndexChanged);
+            // 
+            // listBoxProduitMenu
+            // 
+            this.listBoxProduitMenu.FormattingEnabled = true;
+            this.listBoxProduitMenu.ItemHeight = 15;
+            this.listBoxProduitMenu.Location = new System.Drawing.Point(218, 52);
+            this.listBoxProduitMenu.Name = "listBoxProduitMenu";
+            this.listBoxProduitMenu.Size = new System.Drawing.Size(120, 94);
+            this.listBoxProduitMenu.TabIndex = 6;
+            // 
             // OrderForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBoxProduitMenu);
+            this.Controls.Add(this.listBoxCategories);
             this.Controls.Add(this.buttonPayer);
             this.Controls.Add(this.buttonAttente);
             this.Controls.Add(this.textBoxTotal);
             this.Controls.Add(this.dataGridOrder);
             this.Controls.Add(this.textBoxOrderName);
-            this.Controls.Add(this.listViewMenusProduits);
-            this.Controls.Add(this.listViewCategories);
             this.Name = "OrderForm";
             this.Text = "OrderForm";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridOrder)).EndInit();
@@ -116,13 +119,12 @@
         }
 
         #endregion
-
-        private ListView listViewCategories;
-        private ListView listViewMenusProduits;
         private TextBox textBoxOrderName;
         private DataGridView dataGridOrder;
         private TextBox textBoxTotal;
         private Button buttonAttente;
         private Button buttonPayer;
+        private ListBox listBoxCategories;
+        private ListBox listBoxProduitMenu;
     }
 }
--- Back/OrderController/OrderForm.cs
+++ Back/OrderController/OrderForm.cs
@@ -1,14 +1,7 @@
 ﻿using AssociationCRMDawanPoe.Entity;
+using AssociationCRMDawanPoe.Persistance;
 using AssociationCRMDawanPoe.Service;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace Back.OrderController
 {
@@ -16,18 +9,23 @@ namespace Back.OrderController
     {
         //public IOrderService orderService;
         //public IMenuService menuService;
-        //public IProductService productService;
+        public IProductService productService;
 
-        public List<string> NavigationCategories = new List<string>();
+        //public List<string> NavigationCategories = new List<string>();
 
-        public List<Product> products = new List<Product>();
-        public List<Menu> menus = new List<Menu>();
+        public List<Product> Products = new List<Product>();
+        public List<Menu> Menus = new List<Menu>();
 
         public OrderForm()
         {
+            productService = new ProductServiceImpl(new ProductRepository("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=CRM;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"));
+
+            Products = productService.GetAll();
+
+
             InitializeComponent();
             #region Paramètres d'affichage:
-            
+
             textBoxOrderName.Enabled = false;
             textBoxTotal.Enabled = false;
             textBoxTotal.TextAlign = HorizontalAlignment.Center;
@@ -48,9 +46,48 @@ namespace Back.OrderController
             #endregion
 
             #region Initialisation ListViewCategories
-            NavigationCategories = new List<string>();
+            // NavigationCategories = GetAllCategories();
+            GetAllCategories();
 
+            //listViewCategories.BindingContext = NavigationCategories;
             #endregion
+        }
+
+        #region
+        public void GetAllCategories()
+        {
+            //List<string> categories = new List<string>();
+            //Product p = new();
+            var cat = Enum.GetValues(typeof(ProductCategory)).Cast<ProductCategory>().ToList();
+            foreach (var item in cat)
+            {
+                listBoxCategories.Items.Add(item.ToString());
+            }
+
+            // return categories;
+        }
+
+
+
+        #endregion
+
+        private void listBoxCategories_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBoxProduitMenu.Items.Clear();
+            if (listBoxCategories.SelectedItem is not null)
+            {
+                int i = listBoxCategories.SelectedIndex;
+                textBoxTotal.Text = i.ToString();
+                
+               //TODO Null exception si pas de produit dans la catégorie other.
+
+                List<Product> p = Products.Where(x => x.ProductCategory.ToString() == listBoxCategories.Items[i+1].ToString()).ToList();
+
+                foreach (Product item in p)
+                {
+                    listBoxProduitMenu.Items.Add(item.Name);
+                }
+            }
         }
     }
 }
